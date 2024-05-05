d5bdb414b33eae6a96a4942f83ba90ba00a1e30b
engueranraout <rt.engueran@gmail.com>
2024-05-05 21:25:58 +0200

    fix: front http.ts

--- web-app/src/lib/http.ts
+++ web-app/src/lib/http.ts
@@ -1,3 +1,10 @@
 export function sendGetRequest(url: string): Promise<Record<string, unknown>> {
-  // fetch…
+  return fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((json) => json as Record<string, unknown>);
 }
